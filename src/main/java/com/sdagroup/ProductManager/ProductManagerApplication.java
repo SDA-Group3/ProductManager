05589010f0f57238b22119e07f48f4b736abac6e
AngieSepulveda <147972715+AngieSepulveda@users.noreply.github.com>
2024-04-19 00:01:09 +0300

    remove unnecessary code

--- src/main/java/com/sdagroup/ProductManager/ProductManagerApplication.java
+++ src/main/java/com/sdagroup/ProductManager/ProductManagerApplication.java
@@ -14,25 +14,3 @@ public class ProductManagerApplication {
 
 }
 
-
-
-package com.sdagroup.ProductManager.service;
-
-import com.sdagroup.ProductManager.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ProductService {
-
-    @Autowired
-    private ProductRepository productRepository;
-
-    public void deleteProductById(Long id) {
-        productRepository.deleteById(id);
-    }
-
-    public void deleteAllProducts() {
-        productRepository.deleteAll();
-    }
-}
